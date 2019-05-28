a706aa2b562692f115a0425501d2c32de5f7d329
zhangfengming <763136195@qq.com>
2019-05-28 16:11:57 +0800

    新增dockerfile

--- src/main/java/com/cloud/simple/WebtestApplication.java
+++ src/main/java/com/cloud/simple/WebtestApplication.java
@@ -1,15 +1,36 @@
 package com.cloud.simple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 
-public class WebtestApplication {
+public class WebtestApplication implements ApplicationListener<WebServerInitializedEvent> {
+    private static int serverPort;
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent webServerInitializedEvent) {
+        this.serverPort = webServerInitializedEvent.getWebServer().getPort();
+    }
 
     public static void main(String[] args) {
+        Logger log = LoggerFactory.getLogger(WebtestApplication.class);
+
         SpringApplication.run(WebtestApplication.class, args);
+        try {
+            log.info("ip地址<<<<<<<<<<<<<<<<<<<<<<<<<<<<"+ InetAddress.getLocalHost()+":"+serverPort
+            );
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
